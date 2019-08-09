52ce76ebf28600ab0a8f81dcae78615a5e4992e6
LuiggiTenorioK <luiggi.tenorio@pucp.pe>
2019-08-09 00:00:35 -0500

    Status Code Included

--- app/store/fetchMiddleware.js
+++ app/store/fetchMiddleware.js
@@ -11,16 +11,24 @@ export default function fetchMiddleware({ dispatch, getState }) {
 
         const actionPromise = fetch(promise.url, promise);
 
+        
+
         actionPromise
             .then(response => {
-                if (response.status === 200){
-                    response.json().then( payload => next({...rest, payload, type: SUCCESS}) );
-                } else{
-                    response.json().then( error => next({...rest, error, type: FAILURE}) );
-                }    
+                let _status = response.status;
+                if(response.ok){
+                    response.json()
+                        .then( payload => next({...rest, payload, type: SUCCESS, status: _status}) );
+                } else {
+                    response.json()
+                        .then( payload => next({...rest, payload, type: FAILURE, status: _status}) );
+                }
             })
-            .catch(error => next({...rest, error, type: FAILURE}));
+            .catch( error => next({...rest, error, type: FAILURE, status: 520}) );
+            // Unknown error status 520 
+            // is a "catch-all response for when the origin server returns something unexpected" by Cloudfare (Wikipedia)
 
+        
         // El viejo Jenkins
         /*actionPromise
             .then(response => response.json())
